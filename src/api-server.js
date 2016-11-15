2298d1a25dd2fb172b162397dfdde684e27468ba
Joseph Green <joe.green@foxsoftware.net>
2016-11-14 20:44:01 -0700

    redis setup

--- src/api-server.js
+++ src/api-server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {setupRedis, getClient} = require('./redis-client');
 
 const app = express();
 
@@ -7,6 +8,19 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!-Booya');
 });
+
+app.get('/redis-test', (req, res) => {
+    getClient().incr('inc-test', (err, result) => {
+        if (err) {
+            res.send('Error incrementing redis test');
+        }
+        else {
+            res.send(`Redis increment test result: ${result}`);
+        }
+    });
+});
+
+setupRedis();
 
 app.listen(port, () => {
     console.log('Express app listening on port 3000');
